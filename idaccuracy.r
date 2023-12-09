8c3852015f131b8ec17f71dcaa2c2af3df18ed4d
huangzhiming <2141703@tongji.edu.cn>
2023-12-09 20:05:57 +0800

    third commit

--- idaccuracy.r
+++ idaccuracy.r
@@ -23,3 +23,20 @@ data <- lapply(merged_df, read.csv)
 merged_df <- do.call(rbind, data)
 merged_df <- merged_df[,c("doi", "年份", "期刊", "标题")]
 print(merged_df)
+
+#将2中代码封装成为一个可以在命令行运行的脚本，脚本的唯一一个参数为aminer论文文件所在的路径
+args <- commandArgs(trailingOnly = TRUE)
+path <- args[1]
+
+merge_df <- function(path) {
+  setwd(path)
+  merged_df <- lapply(list.files(), function(file) {
+    data <- read.csv(file, sep = ",", header = TRUE)
+  })
+  merged_df <- do.call(rbind, data)
+  merged_df <- merged_df[,c("doi", "年份", "期刊", "标题")]
+  print(merged_df)
+  write.csv(merged_df, file = "merged_df.csv", sep = ",", row.names = FALSE)
+}
+
+merge_df("/Users/hwangtsimin/R/assignment_idaccuracy/Aminer")
